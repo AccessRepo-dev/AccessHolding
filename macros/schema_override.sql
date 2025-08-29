963d4e2e65e3eda1205738f603ab1325ecec5c69
srinivas.annem@accordion.com <srinivas.annem@accordion.com>
2025-08-29 09:14:05 +0000

    m1

--- macros/schema_override.sql
+++ macros/schema_override.sql
@@ -1,13 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
-    {# 
-      If running in dbt Cloud or CI/CD (DEPLOYMENT_ENV = "prod"),
-      then use the schema from project.yml (e.g., SILVER/GOLD).
-      Otherwise, use the default schema from developer's connection.
-    #}
-
-    {% if env_var('DEPLOYMENT_ENV', 'dev') == 'prod' %}
-        {{ custom_schema_name | upper }}
-    {% else %}
+    {%- if target.name in ['dev', 'default'] -%}
+        -- For developers, use their default schema (whatever their Snowflake user has set)
         {{ target.schema }}
-    {% endif %}
+    {%- else -%}
+        -- For builds / prod jobs, use schema defined in project.yml
+        {{ custom_schema_name | upper }}
+    {%- endif -%}
 {% endmacro %}
